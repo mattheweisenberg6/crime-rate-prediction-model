61961d5539ef17e336c684a76976442ce55895ad
mattheweisenberg6 <121004831+mattheweisenberg6@users.noreply.github.com>
2025-09-04 19:16:22 -0700

    added crimes.csv and output

--- main.py
+++ main.py
@@ -1,34 +1,8 @@
-import tkinter as tk
-from tkinter import ttk
-from tkinter.messagebox import showinfo
+import pandas as pd
 
-def popup_bonus():
-    win = tk.Toplevel()
-    win.wm_title("Window")
-
-    showinfo("Window", "Input")
-
-    button = ttk.Button(win, text="OK", command=win.destroy)
-
-
-def popup_showinfo():
-    showinfo("Window", "Hello World!")
-
-
-class Application(ttk.Frame):
-
-    def __init__(self, master):
-        ttk.Frame.__init__(self, master)
-        self.pack()
-
-        self.button_bonus = ttk.Button(self, text="Bonuses", command=popup_bonus)
-        self.button_bonus.pack()
-
-        self.button_showinfo = ttk.Button(self, text="Show Info", command=popup_showinfo)
-        self.button_showinfo.pack()
-
-root = tk.Tk()
-
-app = Application(root)
-
-root.mainloop()
+df = pd.read_csv('crimes.csv', nrows=100)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
+print(df)
+print(df.columns.tolist())
